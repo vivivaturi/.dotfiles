fe097a0f61f96a7637bfdbe6b60e05537f72f4f2
Vivek Ivaturi <vivek.ivaturi@gmail.com>
2023-04-02 14:37:20 -0400

    Added fugitive

--- nvim/.config/nvim/lua/viv/plugins.lua
+++ nvim/.config/nvim/lua/viv/plugins.lua
@@ -146,6 +146,8 @@ return packer.startup(function(use)
 		"mbbill/undotree",
 		cmd = { "UndotreeToggle", "UndotreeFocus" },
 	})
+
+	use({ "tpope/vim-fugitive" })
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
 	if PACKER_BOOTSTRAP then

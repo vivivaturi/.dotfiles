e96a9b9a937ce2f4cdce286a58ad0272db14fb0a
Vivek Ivaturi <vivek.ivaturi@gmail.com>
2024-01-16 00:29:12 -0500

    Updating telescope filebrowswer

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -36,7 +36,7 @@ return {
       },
       extensions = {
         file_browser = {
-          theme = "ivy",
+          theme = "dropdown",
           -- disables netrw and use telescope-file-browser in its place
           -- hijack_netrw = true,
           hidden = { file_browser = true, folder_browser = true },

6523070e48468f4cd6f9d4f7a4d9044b82e84544
Vivek Ivaturi <vivek.ivaturi@gmail.com>
2025-07-07 20:57:36 -0400

    Updating config with latest

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,34 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local opts = { silent = true }
+
+vim.keymap.set("n", "<Tab>", "<Cmd>BufferLineCycleNext<CR>", opts)
+vim.keymap.set("n", "<S-Tab>", "<Cmd>BufferLineCyclePrev<CR>", opts)
+
+-- Keep curson in the middle when jumping
+vim.keymap.set("n", "<C-d>", "<C-d>zz", opts)
+vim.keymap.set("n", "<C-u>", "<C-u>zz", opts)
+vim.keymap.set("n", "n", "nzzzv", opts)
+vim.keymap.set("n", "N", "Nzzzv", opts)
+
+-- Yank into clipboard
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]], opts)
+vim.keymap.set("n", "<leader>Y", [["+Y]], opts)
+
+-- Move lines in visual mode
+vim.keymap.set("v", "K", ":m-2<CR>gv=gv", opts)
+vim.keymap.set("v", "J", ":m'>+<CR>gv=gv", opts)
+
+-- Indent
+vim.keymap.set("n", "<", "v<", opts)
+vim.keymap.set("n", ">", "v>", opts)
+
+-- Visual --
+-- Stay in indent mode
+vim.keymap.set("v", "<", "<gv", opts)
+vim.keymap.set("v", ">", ">gv", opts)
+
+-- Better paste
+-- vim.keymap.set("v", "p", '"_dP', opts)
--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -1,3 +1,8 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+--
+vim.g.snacks_animate = false
+vim.opt.mouse = "a" -- allow the mouse to be used in neovim
+vim.opt.whichwrap:append("<,>,[,],h,l") -- keys allowed to move to the previous/next line when the beginning/end of line is reached
+vim.opt.clipboard = "unnamedplus" -- allows neovim to access the system clipboard
